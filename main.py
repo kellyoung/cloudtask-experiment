faf1f711857d3d97d9e0aaf4fab30361d48ac990
Kelly <kelly.y94@gmail.com>
2023-10-18 22:13:28 -0700

    try to print body of request

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+# from pydantic import BaseModel
+from fastapi import Request
 
 app = FastAPI()
 
 
 @app.post("/", status_code=200)
-async def root():
-    return {"message": "Hello World"}
+async def process_task(request: Request):
+    payload = await request.body()
+    # Process the payload here
+    print('###PAYLOAD', payload)
+    return {"message": "Payload received and processed"}
